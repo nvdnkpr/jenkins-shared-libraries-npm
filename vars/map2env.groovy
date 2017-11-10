060a8d4e811ffd0060ef97f14b1cf77f34fa74e8
Navid Nikpour <navid@nikpour.com>
2017-11-10 03:16:26 +0100

    Changes recursive envInjection to work within pipeline

--- vars/map2env.groovy
+++ vars/map2env.groovy
@@ -1,20 +1,24 @@
 #!/usr/bin/groovy
 
-def call(map, prefix = ''){
-  map.each { k, v ->
-    def envKey = "${prefix.toUpperCase()}_${k.toUpperCase()}" 
-    switch(v){
-        // value is a string, put it with the key into env
-        case String:
-            env[envKey] = v
-            break
-        // value is a array list, iterate over it, use index for differentiation
-        case java.util.ArrayList:
-            v.eachWithIndex{v2, i -> env["${envKey}_${i}"] = v2 }
-            break
-        // it's a sub object, work recursively
-        default:
-            injectIntoEnv(v, "${prefix}_${k}")
+def call(projectMap, projectPrefix = ''){
+  def injectIntoEnv(map, prefix){
+    map.each { k, v ->
+      def envKey = "${prefix.toUpperCase()}_${k.toUpperCase()}" 
+      switch(v){
+          // value is a string, put it with the key into env
+          case String:
+              env[envKey] = v
+              break
+          // value is a array list, iterate over it, use index for differentiation
+          case java.util.ArrayList:
+              v.eachWithIndex{v2, i -> env["${envKey}_${i}"] = v2 }
+              break
+          // it's a sub object, work recursively
+          default:
+              injectIntoEnv(v, "${prefix}_${k}")
+      }
     }
   }
+
+  injectIntoEnv(projectMap, projectPrefix)
 }
