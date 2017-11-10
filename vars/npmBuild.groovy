5532de338f855beda814ac532fad2c16a1245206
Navid Nikpour <navid@nikpour.com>
2017-11-10 02:50:34 +0100

    Fixes setupTools configuration

--- vars/npmBuild.groovy
+++ vars/npmBuild.groovy
@@ -27,7 +27,7 @@ def call() {
 
         // Setup Tools
         setupTools {
-          "NodeJS 8.7.0" = "bin",
+          "NodeJS 8.7.0" = "bin"
           "SonarQube Scanner 3.0" = "bin"  
         }
       }
